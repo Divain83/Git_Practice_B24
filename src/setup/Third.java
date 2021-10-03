2aaab45f128f004cda041f1e86992f8c340c1c82
Divain83 <iuliiaradnaeva@gmail.com>
2021-10-03 12:54:51 -0400

    made the m method

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -14,5 +14,11 @@ public class Third {
 
     public static void main(String[] args) {
         System.out.println("5");
+        m();
+
+    }
+
+    public static void m (){
+        System.out.println(6);
     }
 }
